306f572d069d9d3e0333ab755010a550b24a07f3
Pavel T <paveltitushkin@gmail.com>
2023-02-22 22:29:17 +0300

    Add OrderAPI

---
+++ Mango.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class AzureServiceBusMessageBus : IMessageBus
+    {
+        //add serviceBus
+        private readonly string connectingString = "...servicebus.windows.com...";
+        public async Task PublishMessageAsync(BaseMessage message, string topicName)
+        {
+            await using var client = new ServiceBusClient(connectingString);
+
+            ServiceBusSender sender = client.CreateSender(topicName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            var finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await sender.DisposeAsync();
+        }
+    }
+}
